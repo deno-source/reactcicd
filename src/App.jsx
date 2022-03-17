027e0278d1804de5b18828df508aba7390655981
transee <1945350177@qq.com>
2022-03-17 15:59:53 +0800

    更新2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Button type="primary">这是个主要按钮</Button>
+      <Button type="primary">2</Button>
     </div>
   );
 }
